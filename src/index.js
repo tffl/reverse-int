7a6132f311307689973ca9cf5448ae034c302603
ntsh <106670334+tffl@users.noreply.github.com>
2024-08-03 16:27:15 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
-}
+      const numToStr = n.toString();
+      const rvrsdStr = numToStr.split('').reverse().join('');
+      return parseInt(rvrsdStr, 10);
+  }
